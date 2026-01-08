ee292f465a63275a57567c03e9f7749c53146ebf
AgS-a <cosmicinfrasilver@gmail.com>
2026-01-08 07:10:56 +0530

    jan7

--- asgmt1/q1e.c
+++ asgmt1/q1e.c
@@ -7,13 +7,30 @@ int main() {
     char ch;
 
     fPtr = fopen("test_ran.dat","r");
-    
+/*    
     do {
         ch = fgetc(fPtr);
         printf("%c",ch);
-    }while(ch!=EOF);
+    }while(ch!=EOF); // EOF -> End of file
+*/
+    
+    double numbers[12];
+    int i=0;
+
+    while(i<10) {
+        fscanf(fPtr,"%lf",&numbers[i]);
+        i++;
+    }
     
     fclose(fPtr);
+   
+   double sum;
+    for(int j=0;j<10;j++) {
+        sum = sum + numbers[j];
+    }
+    double avg;
+    avg = sum/10;
 
+    printf("average is: %f",avg);
     return 0;
 }
