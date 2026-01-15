6737d34ada93e54b57dc6455bb22c782485d77ee
AgS-a <cosmicinfrasilver@gmail.com>
2026-01-15 18:35:44 +0530

    class jan15

--- asgmt2/q3.c
+++ asgmt2/q3.c
@@ -4,18 +4,36 @@
 
 int main() {
     #define frand() ((double) rand() / (RAND_MAX+1.0))
+    
+    FILE*fptr;
+    fptr = fopen("exp.dat","w");
+    
+    int n = 10000;
 
-    double x[10000];
-    for(int i=0;i<10000;i++) {
-        x[i] = frand();
+    double expx[n];
+    for(int i=0;i<n;i++) {
+        expx[i] = exp( ( (-2) * frand() ) );
     }
-    double expx[10000];
-    for(int i=0;i<10000;i++) {
-        expx[i] = exp((-2)*x[i]);
+
+    double gauss[n];
+
+    double mean = 0;
+    double std_dev = 2;
+
+    for(int i=0;i<n;i++) {
+        double rn = frand();
+        rn = pow((rn-mean),2);
+        rn = rn/((-2) * std_dev);
+        rn = pow(exp(1),rn);
+
+        printf("%f\n",rn);
     }
-    for(int i=0;i<10000;i++) {
-        printf("%f\n",expx[i]);
+
+    for(int i=0;i<n;i++) {
+        fprintf(fptr,"%f\n",expx[i]);
     }
+    
+    fclose(fptr);
 
     return 0;
 }
---
+++ asgmt2/q4.c
@@ -0,0 +1,37 @@
+// integrate 4/(1+x**2) from 0 to 1 using trapezoidal method to determine pi
+// actual value: 4tan-1(1)
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+double trap_int_pi(long long int n) {
+    
+    double x[n+1];
+    double y[n+1];
+
+    for(int i=0;i<=n;i++) {
+        x[i] = i * (1/(double)n);
+        y[i] = 4 / (1+(pow(x[i],2)));
+    }
+    
+    double dx = x[n] - x[n-1];
+    double buf = 0;
+    double sum = 0;
+
+    for(int i=0;i<n;i++) {
+        buf = (dx * y[i]) + (0.5 * dx * (y[i+1] - y[i]));
+        sum = sum + buf;
+    }
+    return sum;
+}
+
+int main() {
+    
+    int no_bins[9] = {10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
+    for(int i=0;i<9;i++) {
+        printf("10^%d: %0.20f\n",i+1,trap_int_pi(no_bins[i]));
+    }
+
+    return 0;
+}
