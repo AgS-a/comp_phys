8757f9e0b2e3539ff257322a43ba93f62cf82150
AgS <cosmicinfrasilver@gmail.com>
2026-03-09 16:44:42 +0530

    started asgmt4 prop

--- asgmt4/q1.c
+++ asgmt4/q1.c
@@ -4,14 +4,12 @@
 #include<math.h>
 
 double dx = 0.01;
-double y_init = 1e-8;
+double y_init = 0;
 double x_end = 1.55;
-double y_end = 2; //to be removed
 
 double f_y(double y)
 {
         double fofy = (y * y) + 1;
-        //fofy = 1/sin(y);
         return fofy;
 }
 
@@ -24,11 +22,12 @@ double euler()
 
         double y_n = y_init;
         double x_track = 0;
-        while (x_track < x_end) {
+        while (x_track <= x_end) {
                 y_n = y_n + (f_y(y_n) * dx);
                 fprintf(eul,"%f\n",y_n);
                 x_track += dx;
         }
+        printf("Error in euler:%f\n",fabs(y_n - tan(x_end)));
         fclose(eul);
 }
 
@@ -36,13 +35,23 @@ double modified_euler() // Heun's method //just half the grid size?
 {
         FILE *mod_eul;
         char name_mod_eul[64];
+        sprintf(name_mod_eul,"mod_eul%d.dat",1);
         mod_eul = fopen(name_mod_eul,"w");
 
         double y_n = y_init;
-        while (y_n < y_end) {
+        double x_track = 0;
+        
+        double y_np5 = 0;
+        double m_mid = 0;
+        while (x_track <= x_end) {
+                y_np5 = y_n + (f_y(y_n) * dx * 0.5);
+                m_mid = f_y(y_np5);
+                y_n = y_n + (m_mid * dx);
+
                 fprintf(mod_eul,"%f",y_n);
-                // Heun's method
+                x_track += dx;
         }
+        printf("Error in modified euler:%f\n",fabs(y_n - tan(x_end)));
         fclose(mod_eul);
 }
 
@@ -50,13 +59,27 @@ double improved_euler()
 {
         FILE *imp_eul;
         char name_imp_eul[64];
+        sprintf(name_imp_eul,"imp_eul%d.dat",1);
         imp_eul = fopen(name_imp_eul,"w");
 
         double y_n = y_init;
-        while (y_n < y_end){
-                // Improved euler method
-                fprintf(imp_eul,"%f",y_n);
+        double x_track = 0;
+
+        double m_beg = 0;
+        double m_end = 0;
+        double y_n1 = 0;
+
+        while (x_track <= x_end){
+                m_beg = f_y(y_n);
+                y_n1 = y_n + (m_beg * dx);
+                m_end = f_y(y_n1);
+
+                y_n = y_n + (((m_beg + m_end)/2)*dx);
+
+                fprintf(imp_eul,"%f\n",y_n);
+                x_track += dx;
         }
+        printf("Error in improved euler:%f\n",fabs(y_n - tan(x_end)));
         fclose(imp_eul);
 }
 
@@ -64,14 +87,14 @@ double RK4()
 {
         FILE *RK;
         char name_RK[64];
+        sprintf(name_RK,"RK4%d.dat",1);
         RK = fopen(name_RK,"w");
 
         double k1,k2,k3,k4;
-        //double y_n_minus_1;
         double y_n = y_init;
+        double x_track = 0;
 
-        while (y_n < y_end) {
-                //y_n_minus_1 = y_n;
+        while (x_track <= x_end) {
 
                 k1 = f_y(y_n) * dx;
                 k2 = f_y(y_n + (0.5 * k1)) * dx;
@@ -79,8 +102,10 @@ double RK4()
                 k4 = f_y(y_n + k3) * dx;
 
                 y_n = y_n + (k1 + (2 * k2) + (2 * k3) + k4)/6;
-                fprintf(RK,"%f",y_n);
+                fprintf(RK,"%f\n",y_n);
+                x_track += dx;
         }
+        printf("Error in RK4:%f\n", fabs(y_n - tan(x_end))); 
         fclose(RK);
 }
 
@@ -89,6 +114,9 @@ int main()
         clock_t begin = clock();
         
         euler();
+        modified_euler();
+        improved_euler();
+        RK4();
 
         clock_t end = clock();
         double time_spent = (double)(end-begin) / CLOCKS_PER_SEC;
--- asgmt4/q5.c
+++ asgmt4/q5.c
@@ -2,12 +2,51 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
+
+double dt = 0.01;
+
+double f_x(double x)
+{
+        return -sin(x);
+}
+
+double RK4_solver(double initial_x, double initial_v)
+{
+        int n_iter = 5000
+
+        FILE *RK;
+        char name_RK[64];
+        sprintf(name_RK,"RK4_sinx_x0%.4fv0%.4f.dat",initial_x,initial_v);
+        RK = fopen(name_RK,"w");
+
+        double k1,k2,k3,k4;
+        double x_n = initial_x;
+
+        for (int i=0;i<n_iter;i++) {
+
+                k1 = f_y(y_n) * dt;
+                k2 = f_y(y_n + (0.5 * k1)) * dt;
+                k3 = f_y(y_n + (0.5 * k2)) * dt;
+                k4 = f_y(y_n + k3) * dt;
+
+                y_n = y_n + (k1 + (2 * k2) + (2 * k3) + k4)/6;
+                fprintf(RK,"%f\n",y_n);
+        }
+        fclose(RK);
+}
 
 int main()
 {
         clock_t begin = clock();
+        
+        double x_init = 0.1;
+        double v_init = 1.9;
+        RK_solver(x_init,y_init);
 
-
+        x_init = 0;
+        v_init = 1.99;
+        RK_solver(x_init,v_init);
 
         clock_t end = clock();
         double time_spent = (double)(end-begin) / CLOCKS_PER_SEC;
