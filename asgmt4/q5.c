1e77482b0a678b55893a23876b58a6675aa7e6f2
AgS-a <cosmicinfrasilver@gmail.com>
2026-03-09 17:58:00 +0530

    new res

--- asgmt4/q5.c
+++ asgmt4/q5.c
@@ -20,15 +20,17 @@ double RK4_solver(double initial_x, double initial_v)
         sprintf(name_RK,"RK4_sinx_x0%.4fv0%.4f.dat",initial_x,initial_v);
         RK = fopen(name_RK,"w");
 
-        double k1,k2,k3,k4;
+        double k1_v,k2_v,k3_v,k4_v;
+        double k1_x,k2_x,k3_x,k4_x;
         double x_n = initial_x;
+        double v_n = initial_v;
 
         for (int i=0;i<n_iter;i++) {
 
-                k1 = f_y(y_n) * dt;
-                k2 = f_y(y_n + (0.5 * k1)) * dt;
-                k3 = f_y(y_n + (0.5 * k2)) * dt;
-                k4 = f_y(y_n + k3) * dt;
+                k1_v = f_x(y_n) * dt;
+                k2_v = f_x(y_n + (0.5 * k1)) * dt;
+                k3_v = f_x(y_n + (0.5 * k2)) * dt;
+                k4_v = f_x(y_n + k3) * dt;
 
                 y_n = y_n + (k1 + (2 * k2) + (2 * k3) + k4)/6;
                 fprintf(RK,"%f\n",y_n);
