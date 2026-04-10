cf7ec8566480d5e156350fd14a8b1c810338747f
AgS-a <cosmicinfrasilver@gmail.com>
2026-04-10 22:39:45 +0530

    num_dens_fail

--- asgmt6/q2/q2.c
+++ asgmt6/q2/q2.c
@@ -281,8 +281,8 @@ int main()
 
         srand(time(NULL));
 
-        int n = 2197; // Number of particles
-        double dt = 0.0005; // Integration time step
+        int n = 1200; // Number of particles
+        double dt = 0.005; // Integration time step
         double box_size = 20;
         double epsilon = 1; // Depth of potential minima
         double r_c = 2.5; // Cutoff radius
@@ -321,7 +321,7 @@ int main()
         force_calc(sigma, epsilon, x_position, y_position, z_position, r_c,
                         x_force, y_force, z_force, n, box_size);
 
-        int n_iter = 100000;
+        int n_iter = 30000;
         for(int i=0; i < n_iter; i++){
                 position_update(x_position, y_position, z_position, x_velocity, y_velocity,
                                 z_velocity, x_force, y_force, z_force, dt, mass, n, box_size);
